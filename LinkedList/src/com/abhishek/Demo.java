ebbea711dcdc95224fd802e30101ba893b4eecf8
Abhishek <akthakuron120@gmail.com>
2021-08-11 15:14:38 +0530

    Implementing a program through which we can add city in sorted order

--- LinkedList/src/com/abhishek/Demo.java
+++ LinkedList/src/com/abhishek/Demo.java
@@ -3,25 +3,24 @@ package com.abhishek;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<String >();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
-
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
         printList(placesToVisit);
 
-        placesToVisit.add(1, "Alice Springs");
+        addInOrder(placesToVisit, "Alice Springs");
+        addInOrder(placesToVisit, "Darwin");
         printList(placesToVisit);
-
-        placesToVisit.remove(4);
-        printList(placesToVisit);
+        visit(placesToVisit);
     }
 
     private static void printList(LinkedList<String > linkedList) {
@@ -52,5 +51,71 @@ public class Demo {
             }
         }
         stringListIterator.add(newCity);
+        return true;
+    }
+
+    private static void visit(LinkedList cities) {
+        Scanner sc = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if(cities.isEmpty()) {
+            System.out.println("No cities in the itenerary");
+            return;
+        } else {
+            System.out.println("Now visiting " + listIterator.next());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = sc.nextInt();
+            sc.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Holiday (Vacation) over");
+                    quit =true;
+                    break;
+                case 1:
+                    if(!goingForward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of list");
+                        goingForward = false;
+                    }
+                    break;
+                case 2:
+                    if(goingForward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                        goingForward = true;
+                    }
+                    break;
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions:\nPress");
+        System.out.println("0 - To quit");
+        System.out.println("1 - Go to next city");
+        System.out.println("2 - Go to previous city");
+        System.out.println("3 - Print menu options");
     }
 }
