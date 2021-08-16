1d4ba2bca5db5e7fd2cc539184fe09893b370cfd
Abhishek <akthakuron120@gmail.com>
2021-08-16 20:23:13 +0530

    Implement Player class

--- GenericsPart2/src/com/abhishek/Main.java
+++ GenericsPart2/src/com/abhishek/Main.java
@@ -3,6 +3,21 @@ package com.abhishek;
 public class Main {
 
     public static void main(String[] args) {
-	    
+	    FootballPlayer Ronaldo = new FootballPlayer("Ronaldo");
+	    BaseballPlayer pat = new BaseballPlayer("Pat");
+	    SoccerPlayer Jon = new SoccerPlayer("Jon");
+
+	    Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+	    adelaideCrows.addPlayer(Ronaldo);
+//	    adelaideCrows.addPlayer(pat);
+//	    adelaideCrows.addPlayer(Jon);
+
+        System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> soccerTeam = new Team<>("This won't work");
+        soccerTeam.addPlayer(Jon);
     }
 }
